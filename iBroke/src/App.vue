cf1f33146e9fff11424e1fd8eaaba0100f4bbca4
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-10-31 16:27:42 -0700

    refactor / cleanup + reduce

--- iBroke/src/App.vue
+++ iBroke/src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <main class="p-20 bg-slate-400 my-10">
     <nav class="flex justify-between">
-      <div class="flex items-center">
-        <router-link to="/homee">Home Test</router-link>
-        <router-link to="/options">Options Tracker</router-link>
-        <router-link to="/account">Account Summary</router-link>
+      <div
+        class="flex items-center text-xl border text-blue-800 border-gray-300 hover:text-green-800"
+      >
+        <router-link to="/" class="text-blue-200">Home </router-link>
+        <router-link to="options" class="text-blue-200"
+          >Options Tracker</router-link
+        >
+        <router-link to="account" class="text-blue-200"
+          >Account Summary</router-link
+        >
         <p>About</p>
       </div>
     </nav>
@@ -12,31 +18,13 @@
       <h1 class="text-3xl font-bold text-gray-200 mb-4">
         iBroke Brokerage Tracker
       </h1>
-
       <router-view></router-view>
-
-      <!-- <options-tracker></options-tracker> -->
     </div>
-
-    <!-- <account-summary ref="childRef"></account-summary> -->
   </main>
 </template>
 
 <script lang="ts">
-import { ref } from 'vue'
+//import { ref } from 'vue'
 
-export default {
-  setup() {
-    const childRef = ref(null)
-    const getHistory = () => {
-      if (childRef.value) {
-        childRef.value.getHistory()
-      }
-    }
-    return {
-      childRef,
-      getHistory,
-    }
-  },
-}
+export default {}
 </script>
