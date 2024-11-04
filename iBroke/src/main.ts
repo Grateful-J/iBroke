c656182b67ff6877d2e7fef7989e78c461cedc2b
mrdj863 <107734167+mrdj863@users.noreply.github.com>
2024-11-04 09:06:31 -0500

    enable PrimeVue config

--- iBroke/src/main.ts
+++ iBroke/src/main.ts
@@ -5,6 +5,7 @@ import { createPinia } from 'pinia'
 
 //import PrimeVue from 'primevue/config'
 import 'primeicons/primeicons.css'
+import PrimeVue from 'primevue/config'
 
 import App from './App.vue'
 import router from './router'
@@ -13,5 +14,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue)
 
 app.mount('#app')
